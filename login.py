37ed81b159f09579a0863164018b8d5c94b21587
Peter Tang <peterttang@cs.uchicago.edu>
2020-02-07 17:05:21 -0600

    some attempts

--- login.py
+++ login.py
@@ -1,11 +1,14 @@
 # Python code to log into Strava
 
-#https://www.strava.com/login
-
 import requests
 from lxml import html
+import sys
+import bs4
+import re
+import urlutil
+import csv
 
-session_requests = requests.session()
+# session_requests = requests.session()
 
 #email input
 #<input type="email" name="email" id="email" value="" placeholder="Your Email" autofocus="autofocus" class="form-control">
@@ -15,12 +18,34 @@ session_requests = requests.session()
 
 #csrf token
 #<meta name="csrf-token" content="l3ge0TbJM9KhInAvxl0iCOI4uOLmzM+1ZiZs5j9y6OQvzuHtFX322osc4ipggMdHuoCuEEC4dpA6dHGcdNz83Q==">
+login_url = "https://www.strava.com/login"
+
+login_page = urlutil.get_request(login_url)
+login = urlutil.read_request(login_page)
+soup = bs4.BeautifulSoup(login, 'html.parser')
+utf8 = soup.find_all('input',
+                     {'name': 'utf8'})[0].get('value').encode('utf-8')
+token = soup.find_all('input',
+                      {'name': 'authenticity_token'})[0].get('value')
 
 payload = {
-	"username": "<USER NAME>", 
-	"password": "<PASSWORD>", 
-	"csrfmiddlewaretoken": "l3ge0TbJM9KhInAvxl0iCOI4uOLmzM+1ZiZs5j9y6OQvzuHtFX322osc4ipggMdHuoCuEEC4dpA6dHGcdNz83Q"
+    'utf8': utf8,
+    'authenticity_token': token,
+    'plan': "",
+    'email': "stravascraper123@gmail.com",
+    'password': "2hourmarathon",
 }
+
+REQUEST_URL = "https://www.strava.com/login/session"
+POST_LOGIN_URL = "https://www.strava.com/"
+with requests.Session() as session:
+    post = session.post(POST_LOGIN_URL, data=payload)
+    r = session.get(REQUEST_URL)
+    print(r.text)
+# activity = urlutil.get_request(POST_LOGIN_URL)
+# activity_html = urlutil.read_request(activity)
+# soup = bs4.BeautifulSoup(activity_html, "lxml")
+# print(soup)
 
 
 
