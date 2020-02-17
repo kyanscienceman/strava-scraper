1e4a2151a44ef033c10a718ba80cf1d61ac5c6a1
Kevin Yan <kyan1@cs.uchicago.edu>
2020-02-17 14:21:27 -0600

    Accounted for privacy settings on Strava

--- strava_scraper.py
+++ strava_scraper.py
@@ -15,7 +15,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 BASE_URL = "https://www.strava.com"
-MARATHON_PAGES = {'https://www.strava.com/running_races/2782/results?page={}': ('2019-13-10', 'Chicago')}
+MARATHON_PAGES = {
+    'https://www.strava.com/running_races/2153/results?page={}': ('2018-07-10', 'Chicago'),
+    'https://www.strava.com/running_races/2017-chicago-marathon/results?page={}': ('2018-08-10', 'Chicago'),
+    'https://www.strava.com/running_races/2782/results?page={}': ('2019-13-10', 'Chicago')
+}
 LOGIN_URL = BASE_URL + "/login"
 LOGIN_EMAIL = "stravascraper123@mail.com"
 LOGIN_PASSWORD = "2hourmarathon"
@@ -70,38 +74,43 @@ def strava_scrape(filename):
 
             #Iterate through all marathon results on this page
             activities = soup.find("tbody").find_all("tr")
-            attr_dict = {}
             for a in activities:
                 #Find the url of the activities page for this athlete's run 
                 a_url = a.find("td", class_="athlete-activity").find(href=True)["href"]
                 a_url = urlutil.convert_if_relative_url(BASE_URL, a_url)
 
-                #Navigate to the activities page and check for shoe information
+                #Navigate to the activities page and check for privacy settings 
                 driver.get(a_url)
-                elem = driver.find_element_by_class_name("activity-stats")
+                try:
+                    elem = driver.find_element_by_class_name("activity-stats")
+                except:
+                    continue
+                #Then check if they have shoe information
                 stats = bs4.BeautifulSoup(elem.get_attribute("innerHTML"), 'lxml')
                 shoes = stats.find("span", class_="gear-name")
+                if shoes is None:
+                    continue
 
-                #If so, then get all the information for this athelete's run
-                if shoes is not None:
-                    #Information on the running_races page
-                    attr_dict['Name'] = a.find("a", class_="minimal").text
-                    attr_dict['Gender'] = a.find("td", class_="athlete-gender").text.strip()
-                    attr_dict['Age'] = a.find("td", class_="athlete-age").text
-                    attr_dict['Time1'] = a.find("td", class_="finish-time").text
+                #If the shoes are listed, then get all the information for this run
+                attr_dict = {}
 
-                    #Information on the activities page
-                    shoe_name = re.findall(r"(.+)\s\(", shoes.text.strip())
-                    if shoe_name:
-                        attr_dict['Shoes'] = shoe_name[0]
-                    else:
-                        attr_dict['Shoes'] = None
-                    attr_dict['Time2'] = stats.find_all("li")[1].find("strong").text
+                #Information on the running_races page
+                attr_dict['Name'] = a.find("a", class_="minimal").text
+                attr_dict['Gender'] = a.find("td", class_="athlete-gender").text.strip()
+                attr_dict['Age'] = a.find("td", class_="athlete-age").text
+                attr_dict['Time1'] = a.find("td", class_="finish-time").text
+                #Information on the activities page
+                shoe_name = re.findall(r"(.+)\s\(", shoes.text.strip())
+                if shoe_name:
+                    attr_dict['Shoes'] = shoe_name[0]
+                else:
+                    attr_dict['Shoes'] = None
+                attr_dict['Time2'] = stats.find_all("li")[1].find("strong").text
 
-                    #Write this information to the specified CSV file
-                    with open(filename, 'a') as csvfile:
-                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
-                        writer.writerow(attr_dict)
+                #Write this information to the specified CSV file
+                with open(filename, 'a') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
+                    writer.writerow(attr_dict)
 
             #Move to the next page of marathon results
             page_num += 1
@@ -111,97 +120,3 @@ def strava_scrape(filename):
     driver.close()
 
 strava_scrape("strava.csv")
-
-# def go():
-#     '''
-#     Function that scrapes all 
-#     '''
-#     '''We will have to manually input a list of Strava URLs to scrape
-#     (I don't think there is a way around this).
-#     This should probably come from a JSON file that we upload to the repository.
-#     In that file we could store information something like this:
-#     {
-#         URL1: (2019, 'Chicago'),
-#         URL2: (2019, 'New York'),
-#         URL3: (2017, 'Chicago'),
-#         URL4: (2016, 'Boston')
-#     }
-#     where each key is a url, and each value is a tuple of (year, marathon city).
-#     For the time being, let's hard code the JSON file as a Python dictionary.'''
-#     marathon_homepages = {'https://www.strava.com/running_races/2782/results?page={}': ('2019-13-10', 'Chicago')}
-
-#     #Set up the CSV file with the header so we can append to it later
-#     with open("strava.csv") as csvfile:
-#         fieldnames = ["Name", "Gender", "Age", "Time1", "Time2", "Shoes"]
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
-#         writer.writeheader()
-
-#     '''Eventually, we will have a big dictionary of starting URLs, and we
-#     want to scrape the data for the marathon corresponding to each of them.
-#     Luckily Strava URLs are formatted super neatly, so we can just use 
-#     string formatting instead of searching the page for links.'''
-#     for url, (year, city) in marathon_homepages.items():
-#         page_num = 1
-#         last_page_num = sys.maxsize
-
-#         while page_num <= last_page_num:
-#             page_url = url.format(page_num)
-#             page = urlutil.get_request(page_url)
-#             page_html = urlutil.read_request(page)
-#             soup = bs4.BeautifulSoup(page_html, "lxml")
-
-#             '''On the first page we have an additional task:
-#             find the last page number that we need to scrape for this marathon.
-#             For example, Chicagp 2019 has 693 pages of information,
-#             so in this step we set last_page_num = 693.'''
-#             if page_num == 1:
-#                 li = soup.find("li", class_="next_page")
-#                 n = li.previous_sibling.previous_sibling.findChild().text
-#                 last_page_num = int(n)
-
-#             #Now, scrape all the running data from this page.
-#             activities = soup.find("tbody").find_all("tr")
-#             for a in activities:
-#                 attr_dict = {}
-#                 attr_dict['Name'] = a.find("a", class_="minimal").text
-#                 attr_dict['Gender'] = a.find("td", class_="athlete-gender").text.strip()
-#                 attr_dict['Age'] = a.find("td", class_="athlete-age").text
-#                 attr_dict['Time1'] = a.find("td", class_="finish-time").text
-#                 activity_url = a.find("td", class_="athlete-activity").find(href=True)["href"]
-#                 activity_url = urlutil.convert_if_relative_url(page_url, activity_url)
-#                 print(attr_dict, activity_url)
-#                 #Write all of this information to the output CSV file
-#                 scrape_activity(activity_url, attr_dict)
-
-#             page_num += 1
-
-
-# def scrape_activity(activity_url, attr_dict):
-#     '''
-#     Scrapes a Strava activity page for running data
-    
-#     Inputs:
-#         activity_url (string): URL of the Strava page to scrape
-#         attribute_dict (dict): other relevant data from the home page
-
-#     Returns:
-#         None, but appends to the strava.csv file
-#     '''
-#     activity = urlutil.get_request(activity_url)
-#     activity_html = urlutil.read_request(activity)
-#     soup = bs4.BeautifulSoup(activity_html, "lxml")
-
-#     #Get running time from activity page
-#     stats = soup.find("ul", class_="inline-stats section")
-#     time2 = stats.find("strong").text
-#     attr_dict['Time2'] = time2
-
-#     #Get shoes from activity page
-#     shoes = soup.find("span", class_="gear-name").text
-#     attr_dict['Shoes'] = shoes
-
-#     with open("strava.csv", 'a') as csvfile:
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
-#         writer.writerow(attr_dict)
-        
-# go()
