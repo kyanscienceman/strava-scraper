93da8d23a61c41bc4d8af2291ae5c9c4ec16a695
Peter Tang <peterttang@cs.uchicago.edu>
2020-03-08 22:19:08 -0500

    vaporfly tracker complete

--- training_data_shoes.py
+++ training_data_shoes.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+# scratch code to test
 testing_set = ['Nike Vaporfly Next% Pink (10.0)',
  'Nike Next % pink',
  'Nike Vaporfly Next (#2)',
@@ -41,16 +44,45 @@ testing_set = ['Nike Vaporfly Next% Pink (10.0)',
  'Nike Zoom Vaporfly Next% - Lime',
  'Nike Vaporfly 4% (Red)']
 
-checks = ["%", "vf", "fly", "next", "vapor", "vapour"]
+CHECKS = ["%", "vf", "fly", "next", "vapor", "vapour", "percent"]
 
 final_lst = []
 
 for shoe in testing_set:
-	for check in checks:
-		if check in shoe.lower():
-			final_lst.append(shoe) # might be more efficient to 
-			# find a way to break here if there is a match
+    for check in CHECKS:
+        if check in shoe.lower():
+            final_lst.append(shoe) # might be more efficient to 
+            # find a way to break here if there is a match
 
 final_lst = set(final_lst)
 
 print("Success rate is: ", len(final_lst)/len(testing_set))
+
+# real code let's go bois
+
+def find_vaporfly(filename):
+    '''
+    Inputs:
+        filename (string): name of file
+    Returns:
+        dataframe (df): dataframe with Vaporfly identified in new column
+            identification as True for Vaporfly, False for non-Vaporfly shoes
+            and None for no shoes inputted in Strava
+
+    '''
+    marathon_df = pd.read_csv(filename, sep="|")
+    marathon_df["Vaporfly"] = None
+    row_count = marathon_df.shape[0]
+
+    for i in range(row_count):
+        shoe = marathon_df.iloc[i,6]
+        vaporfly = marathon_df.iloc[i,7]
+        # shoes will temporarily be 6th column
+        # vaporfly will temporarily be 7th column
+        if pd.isnull(shoe) is False:
+            marathon_df.iloc[i,7] = False
+            for check in CHECKS:
+                if marathon_df.iloc[i,7] is False:
+                    if check in shoe.lower():
+                        marathon_df.iloc[i,7] = True
+    return marathon_df
