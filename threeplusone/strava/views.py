8c21040edcdc94c699059f007ef5f8103f90345a
Kevin Yan <kyan1@cs.uchicago.edu>
2020-03-09 14:11:59 -0500

    more changes to website layout

--- threeplusone/strava/views.py
+++ threeplusone/strava/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from django import forms
 from regression import average_marathon_time
 
-def home(request):
-    return render(request, 'strava/home.html', {})
-
 class SearchForm(forms.Form):
     age = forms.IntegerField(label='age', required=False)
     sex = forms.ChoiceField(label='sex', choices=[('M', 'M'), ('F', 'F')], required=False)
+
+def home(request):
+    return render(request, 'strava/home.html', {})
 
 def input(request):
     return render(request, 'strava/input.html', {'form': SearchForm()})
@@ -18,12 +18,17 @@ def results(request):
     if request.method == 'GET':
         if not request.GET:
             context['error'] = "Sorry, you didn't conduct a search. Try again?"
-        else:
+            context['form'] = SearchForm()
+            return render(request, 'strava/input.html', context)
+        age = None
+        sex = None
+        if request.GET['age']:
             age = int(request.GET['age'])
-            sex = request.GET['sex']
             context['age'] = age
+        if request.GET['sex']:
+            sex = request.GET['sex']
             context['sex'] = sex
-            context['avg_time'] = average_marathon_time(age=age, sex=sex)
-    return render(request, 'strava/results.html', context)
+        context['avg_time'] = average_marathon_time(age=age, sex=sex)
+        return render(request, 'strava/results.html', context)
 
 
