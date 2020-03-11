5014ec28f12e63890080277e83408c349eeb4601
Joey Cipriano <ciprianoj@cs.uchicago.edu>
2020-03-11 16:58:49 -0500

    function is done but could be made more efficient

---
+++ scraping/multiple_races.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import jellyfish
+
+def find_consistent_runners(acceptable_name_score=0.85):
+    '''
+    Takes in the list of matched runners and produces a list of people who ran multiple races
+
+    Inputs:
+        None (only uses the matches dataframe)
+    Returns:
+        consistent_runners (DataFrame) the df of runners who have ran multiple races
+    '''
+
+    matches = pd.read_csv('race_result/master_matches.csv')
+    matches_indexes = []
+
+    for p_index, p_row in matches.iterrows():
+        primary_time = p_row.at['Time']
+        primary_name = p_row.at['Name']
+        primary_gender = p_row.at['Gender']
+        primary_age_lower = p_row.at['Age_Lower']
+        primary_age_upper = p_row.at['Age_Upper']
+        primary_raceid = p_row.at['RaceID']
+
+        searchable_matches = matches[
+            (matches['RaceID'] != primary_raceid) & 
+            (matches['Gender'] == primary_gender) & 
+            (matches['Time'] <= primary_time + 1200) & 
+            (matches['Time'] >= primary_time - 1200) &
+            (matches['Age_Lower'] <= primary_age_upper)]
+
+        # (matches['Age_Upper'] <= primary_age_upper) <- want to add this as well but I don't know how to do & for all those tuples and either or of the last two stipulations
+
+        print(p_index)
+
+        for s_index, s_row in searchable_matches.iterrows():
+            secondary_name = s_row.at['Name']
+
+            name_score = jellyfish.jaro_winkler(primary_name, secondary_name)
+
+            if name_score >= acceptable_name_score:
+                matches_indexes = []
+
+    consistent_runners = matches[matches_indexes]
+
+    return consistent_runners
