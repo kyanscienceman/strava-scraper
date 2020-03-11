e82bec49d421f25668df56de221ab0775780e4a5
Kevin Yan <kyan1@cs.uchicago.edu>
2020-03-10 19:07:43 -0500

    Made changes to url names, added graphical displays, prettified the webpage

--- threeplusone/strava/urls.py
+++ threeplusone/strava/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('input', views.input, name='input'),
+    path('search', views.input, name='input'),
     path('results', views.results, name='results'),
 ]
